c33cf5de429baacf24322d0e0186f925ecfd2120
Aviv Rozenboim <rubysrubys@gmail.com>
2023-07-04 01:27:07 +0300

    Log feed generation requesters

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -1,8 +1,10 @@
 import { InvalidRequestError } from '@atproto/xrpc-server';
 import { AtUri } from '@atproto/uri';
+import jws from 'jws';
 import { Server } from '../lexicon';
 import { AppContext } from '../config';
 import algos from '../algos';
+import logger from '../logger';
 
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.getFeedSkeleton(async ({ params, req }) => {
@@ -19,10 +21,24 @@ export default function (server: Server, ctx: AppContext) {
       );
     }
 
+    const did = parseBearer(req.header('authorization'));
+    if (did) {
+      logger.info('Generating feed for %s', did);
+    }
+
     const body = await algo(ctx, params);
     return {
       encoding: 'application/json',
       body: body,
     };
   });
+}
+
+function parseBearer(authorizationHeader?: string) {
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+    return null;
+  }
+
+  const token = authorizationHeader.substring('Bearer '.length).trim();
+  return jws.decode(token)?.payload?.iss;
 }
