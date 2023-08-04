66957a8eaa4d9a2b35e9899c527e479214c51703
Yarden Laifenfeld <ylaifenfeld@gmail.com>
2023-08-05 01:14:23 +0300

    Fix userlists

--- src/util/userlists.ts
+++ src/util/userlists.ts
@@ -1,4 +1,4 @@
-const FILTERED_USERS = [].concat(NEWS_USERS)
-const NEWS_USERS = [
-	"did:plc:4hm6gb7dzobynqrpypif3dck" // news-feed.bsky.social
+export const NEWS_USERS = [
+  'did:plc:4hm6gb7dzobynqrpypif3dck', // news-feed.bsky.social
 ]
+export const FILTERED_USERS = [...NEWS_USERS]
