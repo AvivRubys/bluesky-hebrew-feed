de5e77eba42db46d12a37573331e8a11a855480b
Aviv Rozenboim <rubysrubys@gmail.com>
2023-08-22 17:12:11 +0300

    Ignore hebrew bot

--- src/util/userlists.ts
+++ src/util/userlists.ts
@@ -1,4 +1,3 @@
-export const NEWS_USERS = [
-  'did:plc:4hm6gb7dzobynqrpypif3dck', // news-feed.bsky.social
-]
-export const FILTERED_USERS = [...NEWS_USERS]
+export const NEWS_FEED_DID = 'did:plc:4hm6gb7dzobynqrpypif3dck'; // news-feed.bsky.social
+export const FEED_BOT_DID = 'did:plc:ioo5uzicjxs5i6nfpjcxbugg'; // bot.avivr.dev
+export const FILTERED_USERS = [NEWS_FEED_DID, FEED_BOT_DID];
