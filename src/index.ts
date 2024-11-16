170e260958f74644fad4821c536a5a098d19b0aa
Aviv <rubysrubys@gmail.com>
2024-11-16 19:38:15 +0100

    Remove FeedGenerator class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config';
-import FeedGenerator from './server';
+import { runFeedGenerator } from './server';
 import { parseEnvironment } from './config';
 import logger from './logger';
 
 async function run() {
   const config = parseEnvironment();
-  const server = FeedGenerator.create(config);
-  await server.start();
+  await runFeedGenerator(config);
   logger.info(
     'Running feed generator at http://%s:%d',
     config.HOST,
--- src/server.ts
+++ src/server.ts
@@ -1,70 +1,41 @@
-import http from 'http';
 import events from 'events';
-import express from 'express';
 import { AtpAgent } from '@atproto/api';
-import { createDb, Database, migrateToLatest } from './db';
+import { createDb, migrateToLatest } from './db';
 import { BlockService } from './blocks';
 import { FirehoseSubscription } from './subscription';
 import { Config } from './config';
 import { createApi } from './api';
-import { AppContext } from './context';
 import { runNotifyBot } from './notify-bot';
 
-export class FeedGenerator {
-  public server?: http.Server;
+export async function runFeedGenerator(cfg: Config): Promise<void> {
+  // Create
+  const db = createDb(cfg);
+  const firehose = new FirehoseSubscription(
+    db,
+    cfg.FEEDGEN_SUBSCRIPTION_ENDPOINT,
+  );
+  const bsky = new AtpAgent({ service: cfg.BLUESKY_API_ENDPOINT });
+  const block = new BlockService(bsky, cfg);
 
-  constructor(
-    private ctx: AppContext,
-    private app: express.Application,
-    private db: Database,
-    private firehose: FirehoseSubscription,
-    private cfg: Config,
-  ) {
-    this.app = app;
-    this.db = db;
-    this.firehose = firehose;
-    this.cfg = cfg;
-  }
+  const ctx = {
+    db,
+    cfg,
+    block,
+    firehose,
+    bsky,
+  };
 
-  static create(cfg: Config) {
-    const db = createDb(cfg);
-    const firehose = new FirehoseSubscription(
-      db,
-      cfg.FEEDGEN_SUBSCRIPTION_ENDPOINT,
-    );
-    const bsky = new AtpAgent({ service: cfg.BLUESKY_API_ENDPOINT });
-    const block = new BlockService(bsky, cfg);
+  const app = createApi(ctx);
 
-    const ctx: AppContext = {
-      db,
-      cfg,
-      block,
-      firehose,
-      bsky,
-    };
+  // Run
+  await migrateToLatest(db);
+  await ctx.bsky.login({
+    identifier: cfg.BLUESKY_CLIENT_LOGIN_IDENTIFIER,
+    password: cfg.BLUESKY_CLIENT_LOGIN_PASSWORD,
+  });
 
-    const app = createApi(ctx);
-
-    return new FeedGenerator(ctx, app, db, firehose, cfg);
-  }
-
-  async loginToBskyAgent() {
-    await this.ctx.bsky.login({
-      identifier: this.ctx.cfg.BLUESKY_CLIENT_LOGIN_IDENTIFIER,
-      password: this.ctx.cfg.BLUESKY_CLIENT_LOGIN_PASSWORD,
-    });
-  }
-
-  async start() {
-    await migrateToLatest(this.db);
-    await this.loginToBskyAgent();
-
-    this.firehose.run(this.cfg.SUBSCRIPTION_RECONNECT_DELAY);
-    this.server = this.app.listen(this.cfg.PORT, this.cfg.HOST);
-    runNotifyBot(this.ctx);
-    await events.once(this.server, 'listening');
-    return this.server;
-  }
+  void firehose.run(cfg.SUBSCRIPTION_RECONNECT_DELAY);
+  const server = app.listen(cfg.PORT, cfg.HOST);
+  runNotifyBot(ctx);
+  await events.once(server, 'listening');
 }
-
-export default FeedGenerator;
