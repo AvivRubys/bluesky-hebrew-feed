4c04304e7f2ae80019e6c23e8bdc08ac255bf352
Aviv Rozenboim <AvivRubys@users.noreply.github.com>
2024-11-21 21:59:21 +0000

    Disable config filtered users

--- src/algos.ts
+++ src/algos.ts
@@ -61,11 +61,11 @@ function createLanguageFeed(
       .orderBy('cid', 'desc')
       .limit(params.limit);
 
-    if (Array.isArray(ctx.cfg.FILTERED_USERS)) {
-      logger.info("Constant filtered users - " + FILTERED_USERS);
-      logger.info("Config filtered users - " + ctx.cfg.FILTERED_USERS.join(','));
-      builder = builder.where('author', 'not in', ctx.cfg.FILTERED_USERS);
-    }
+    // if (Array.isArray(ctx.cfg.FILTERED_USERS)) {
+    //   logger.info("Constant filtered users - " + FILTERED_USERS);
+    //   logger.info("Config filtered users - " + ctx.cfg.FILTERED_USERS.join(','));
+    //   builder = builder.where('author', 'not in', ctx.cfg.FILTERED_USERS);
+    // }
 
     if (includeReplies) {
       if (actor) {
