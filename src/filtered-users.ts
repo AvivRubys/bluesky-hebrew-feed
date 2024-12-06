3b2a0d04f40c601c5dc344cc1355d01a71a30475
Aviv <rubysrubys@gmail.com>
2024-12-06 23:37:37 +0100

    Fixed filtered users

--- src/filtered-users.ts
+++ src/filtered-users.ts
@@ -1,28 +1,26 @@
 import AtpAgent from '@atproto/api';
 import logger from './logger';
-import { Config } from './config';
 import { Database } from './db';
 import { interval } from 'ix/asynciterable';
-import { secondsToMilliseconds } from 'date-fns';
+import { minutesToMilliseconds } from 'date-fns';
 
 const FILTERED_USERS_BY_REQUEST_POST =
   'at://did:plc:ioo5uzicjxs5i6nfpjcxbugg/app.bsky.feed.post/3lbf7z7lb6c2x';
 
-const UPDATE_INTERVAL_MINUTES = secondsToMilliseconds(30);
+const UPDATE_INTERVAL_MINUTES = minutesToMilliseconds(5);
 
 export async function filteredUsersUpdater(bsky: AtpAgent, db: Database) {
   logger.info('Starting filtered users updater');
 
+  await updateFilteredUsers(bsky, db);
   for await (const _ of interval(UPDATE_INTERVAL_MINUTES)) {
-    logger.info('Updating filtered users');
-
     await updateFilteredUsers(bsky, db);
-
-    logger.info('Finished updating filtered users');
   }
 }
 
 async function updateFilteredUsers(bsky: AtpAgent, db: Database) {
+  logger.info('Updating filtered users');
+
   const users = await fetchFilteredUsers(bsky);
   logger.info({ users }, 'Fetched filtered users');
 
@@ -34,6 +32,8 @@ async function updateFilteredUsers(bsky: AtpAgent, db: Database) {
       .values(users.map((did) => ({ did })))
       .execute();
   });
+
+  logger.info('Finished updating filtered users');
 }
 
 async function fetchFilteredUsers(bsky: AtpAgent): Promise<readonly string[]> {
