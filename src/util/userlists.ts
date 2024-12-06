545ecb4e7b9edc7e0e265ec8ce804144cd12acc5
Aviv <rubysrubys@gmail.com>
2024-12-06 23:53:14 +0100

    Moved a bunch of filters that were in config into userlists.ts

--- src/util/userlists.ts
+++ src/util/userlists.ts
@@ -1,7 +1,16 @@
-const NEWS_FEED_DID = 'did:plc:4hm6gb7dzobynqrpypif3dck'; // news-feed.bsky.social
 const FEED_BOT_DID = 'did:plc:ioo5uzicjxs5i6nfpjcxbugg'; // bot.avivr.dev
+
 const SPAM = [
   'did:plc:aest7xbdwd7twym3pqhyihkf', // mokseoyoon.bsky.social
   'did:plc:6tyoa26a4isxgxujdnmzlttg', // mishavanmollusq.bsky.social
+  'did:plc:4hm6gb7dzobynqrpypif3dck', // news-feed.bsky.social
+  'did:plc:l6fkbpyc72a5mmoh4jwxuw2r', // shedim.org
+  'did:plc:4xi4clnyqn2z7hnu6mtlwf7q', // newsil.bsky.social
 ];
-export const FILTERED_USERS = [NEWS_FEED_DID, FEED_BOT_DID, ...SPAM];
+
+const TWITTER_REPOSTERS = [
+  'did:plc:54jnwvnp7arbuh65i7ulg6gh', // yinonmagal1.bsky.social
+  'did:plc:bktiotjbtrqnijcbmpqxzrdo', // abunur.bsky.social - idan landau
+];
+
+export const FILTERED_USERS = [FEED_BOT_DID, ...SPAM, ...TWITTER_REPOSTERS];
